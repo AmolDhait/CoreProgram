65c2381369fc147f43365a7aee91aa8ceac1d102
AmolDhait <amol.dhait97@gmail.com>
2022-03-05 11:01:50 +0530

    Greeting Message

---
+++ QuotientAndRemaider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    public class QuotientAndRemaider
+    {
+        public static void CalculateQuotientAndRemaider()
+        {
+            int dividend = 50;
+            int divsor = 8;
+
+            int quptient = dividend / divsor;
+            int remainder = dividend % divsor;
+            Console.WriteLine("Dividend: {0} Divisorn: {1}", dividend, divsor);
+            Console.WriteLine("QUOTIENT :{0}", quptient);
+            Console.WriteLine("Remainder :{0}", remainder);
+        }
+    }
+}
