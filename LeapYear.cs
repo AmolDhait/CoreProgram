ad9d3624c71c99b9aeb3545c6284d04678e5e49e
AmolDhait <amol.dhait97@gmail.com>
2022-03-05 09:44:14 +0530

    Greeting Message

---
+++ LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    public class LeapYear
+    {
+        public static void DisplayLeap()
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+            {
+                Console.WriteLine("This is a Leap Year.", Year);
+            }
+            else
+            {
+                Console.WriteLine("This is not a Leap Year.", Year);
+                Console.ReadLine();
+            }
+        }
+    }
+}
