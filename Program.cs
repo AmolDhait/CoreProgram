677ddf43d51f8187ec9507fb2fd669bce511af37
AmolDhait <amol.dhait97@gmail.com>
2022-03-05 10:19:06 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CoreProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            //QuotientAndRemaider.CalculateQuotientAndRemaider();
+            //SwapTwoNumber.SwapNumber();
+            //LargestNumber.FindLargestNumber();
+            //EvenAndOddNumber.FindEvenAndOddNumber();
+            FindVowel.FindVowelOrNot();
+            //LeapYear.DisplayLeap();
+            //Fact.FactNumber();
+            //flipCoin.CalculatePercentage();
+            //HarmonicNumber.HarmonicNumberPrint();
+            //PowerOfTwo.Power();
+        }
+    }
+}
