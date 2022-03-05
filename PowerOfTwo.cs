a7b14ec5249f6ed2854c0515ffa87bba762c74df
AmolDhait <amol.dhait97@gmail.com>
2022-03-05 10:48:30 +0530

    Greeting Message

---
+++ PowerOfTwo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    public class PowerOfTwo
+    {
+        public static void Power()
+        {
+            Console.WriteLine("Enter Rang of Power Of Two");
+            int i;
+            int num = Convert.ToInt32(Console.ReadLine());
+            int p = 1;
+            if (num <= 31)
+            {
+                for (i = 0; i <= num; i++)
+                {
+                    if (i == 0)
+                    {
+                        p = 1;
+                    }
+                    else
+                    {
+                        p = p * 2;
+                        Console.WriteLine(p);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Rang of Power Of Two more than 31");
+            }
+        }
+    }
+}
